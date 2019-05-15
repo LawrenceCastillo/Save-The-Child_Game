38da65328a9ca1a027ce39f581bbf42faf3e5480
giocare <jessica.adjei102@gmail.com>
2019-05-15 01:53:17 -0400

    Integrated New Title Scene + Story + RandomGenerated Diseases

---
+++ Assets/DiseasePicker.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DiseasePicker : MonoBehaviour
+{
+   public string[] levelNames;
+
+    public void DiseaseScene()
+    {
+        SceneManager.LoadScene(levelNames[Random.Range(0,3)]);
+    }
+
+
+
+
+}
