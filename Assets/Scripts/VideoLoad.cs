a17971626b55828bb2a0929c120e2c18fd72a540
LawrenceCastillo <lawrence.g.castillo@gmail.com>
2019-05-16 16:15:17 -0400

    Video clip and script

---
+++ Assets/Scripts/VideoLoad.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VideoLoad : MonoBehaviour {
+    public GameObject videoPlayer;
+
+    private void Start(){
+        videoPlayer.SetActive(false);
+    }
+
+    void Update(){
+        if (1)      // Player Wins then play video
+            StartCoroutine(playVideo());
+
+        else if (0){// Player loses 
+            // load "doors" screen
+        }
+    }
+
+    IEnumerator playVideo()
+    {
+        yield return new WaitForSeconds (0);
+        videoPlayer.SetActive(true);
+        yield return new WaitForSeconds (5);
+        videoPlayer.SetActive(false);
+        // load next level
+    }
+}
